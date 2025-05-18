7bb2b172874fdc4a349e7700f4818c26b2b6af42
lucienfradet <lucien.fradet@hotmail.com>
2025-05-17 20:31:00 -0400

    updating wp content

--- backend/cron/entrypoint.sh
+++ backend/cron/entrypoint.sh
@@ -7,11 +7,8 @@ apk add --no-cache curl
 # Setup cron job
 echo "Setting up cron job..."
 echo "# Run cleanup job every hour" > /etc/crontabs/root
-echo "0 * * * * curl -s -X GET -H \"x-api-key: ${CRON_SECRET_KEY}\" ${NEXTJS_URL}/api/cron/cleanup-expired-intents >> /var/log/cron.log 2>&1" >> /etc/crontabs/root
+echo "0 * * * * curl -s -X GET -H \"x-api-key: \${CRON_SECRET_KEY}\" \${NEXTJS_URL}/api/cron/cleanup-expired-intents 2>&1" >> /etc/crontabs/root
 echo "" >> /etc/crontabs/root  # Empty line required at end
-
-# Make sure log file exists
-touch /var/log/cron.log
 
 # Start crond in foreground
 echo "Starting crond..."
